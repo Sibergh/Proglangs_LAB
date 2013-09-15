fa6ab42e328e33c4123cd7832c7428ef68ac487c
Sigve Bergh <sigvebergh@hotmail.com>
2013-09-15 19:55:22 +0200

    Oppdaterte JS filen for LAB2
    Koden jeg brukte i LAB1 har jeg byttet ut med koden som Janis brukte.
    Dette gjorde jeg for å teste ut forskjellen, hvilket ikke eksisterte.
    For å gi brukeren en respons i oppgave 2, så valgte jeg å lage en Alert
    funksjon når brukeren klikket på linkene som er "under konstruksjon".

--- proglangs.js
+++ proglangs.js
@@ -1,6 +1,7 @@
+/*
 //Denne koden ser ut til å fungere på samme måte
-//som den koden du viste i timen (06.09.13).
-//Usikker på hva forskjellen er. //
+//som den koden du viste i timen (06.09.13)//
+//Denne koden brukte jeg på første LAB oppgave, men byttet til koden lenger nede for LAB2//
 
 
 //Når listen forandres, skjer følgende funksjon//
@@ -13,3 +14,21 @@ list.onchange = function() {
     outputbox.innerHTML = "Du har selektert programmeringsspråk: " + this.value;
 	
   };
+*/
+
+//Følgende funksjon ble, ut av kuriositet, brukt for LAB2. Viser seg at den fungerte på 
+//akkurat samme måte som min kode brukt i LAB1.
+function printToOutputBox(selectobj)	{
+	var e_proglangs = document.getElementById("proglangs");
+	var e_outputbox = document.getElementById("outputbox");
+	e_outputbox.innerHTML = "Du har selektert programmeringsspråk: <p>"
+	+ e_proglangs.options[selectobj.selectedIndex].text;
+}
+
+//Følgende kode blir brukt for å opprette et alert vindu når brukeren klikker
+//på linkene "Kronologisk", "Hierarkisk" og "Alfabetisk".
+function AlertIt() {
+var answer = confirm("Siden du ønsker å gå inn på er for øyeblikket under konstruksjon, vennligst kom tilbake senere")
+if (answer)
+window.location="proglangs.html";
+}
